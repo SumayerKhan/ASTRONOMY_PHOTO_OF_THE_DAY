f0fac492c6994834ba6c5f14a146103753febc29
Sumayer Khan Sajid <139729797+SumayerKhan@users.noreply.github.com>
2025-09-08 12:31:38 +0600

    show title,photo name and date

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
@@ -11,3 +12,10 @@ url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 # DATA FETCH
 response = requests.get(url)
 data = response.json()
+
+#STREAMLIT UI
+st.title("Astronomy photo of the day!")
+st.header(data['title'])
+st.write(data['date'])
+
+
