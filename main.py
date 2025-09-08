875f49187c3a0fa7a07ed348f9cacfd1c37608f6
Sumayer Khan Sajid <139729797+SumayerKhan@users.noreply.github.com>
2025-09-08 12:48:56 +0600

    added date pickup ability

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ import requests
 import os
 from dotenv import load_dotenv
 import streamlit as st
+from datetime import date
 
 load_dotenv()
 
 # NASA API
 api_key = os.getenv("API_KEY")
-url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
-
-# DATA FETCH
-response = requests.get(url)
-data = response.json()
 
 #STREAMLIT UI
 st.title("Astronomy photo of the day!")
+chosen_date = st.date_input("Pick a date", value=date.today(), min_value=date(1995, 6, 16), max_value=date.today())
+
+# DATA FETCH
+url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={chosen_date}"
+response = requests.get(url)
+data = response.json()
+
+# Title and date
 st.header(data['title'])
 st.write(data['date'])
-
 #PHOTO FETCH
 img_url = data['url']
 
