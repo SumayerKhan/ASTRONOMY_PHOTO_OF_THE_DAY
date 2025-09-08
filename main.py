083dbfe19f6a15a078c73a847f42ceadfc15a96b
Sumayer Khan Sajid <139729797+SumayerKhan@users.noreply.github.com>
2025-09-08 12:27:24 +0600

    Comment added

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# NASA API
 api_key = os.getenv("API_KEY")
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
+# DATA FETCH
 response = requests.get(url)
 data = response.json()
-img_data = requests.get(data['url']).content
-with open('image.jpg', 'wb') as im:
-    im.write(img_data)
